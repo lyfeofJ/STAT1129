a78a74ffa489513be0adaa3b8e78ef4c1f83426c
lyfeofJ <112637298+lyfeofJ@users.noreply.github.com>
2022-09-20 00:58:07 -0400

    Add files via upload

---
+++ introcomp1.py
@@ -0,0 +1,30 @@
+nums = list(range(30,65,5))
+
+nums.sort(reverse=True)
+
+x = 65
+nums.insert(0,x)
+ 
+print(nums)
+
+
+e_list = []
+for number in range(0,21):
+    e_list += [number]
+
+e_list.remove(0)
+
+print(e_list)
+print(len(e_list))
+print(e_list[-1])
+print(e_list[0])
+
+Sum=sum(e_list)
+print(Sum)
+
+weather={"sunny":"play","rainy":"watch TV","cloudy":"walk"}
+for key, value in weather.items():
+
+        print("When",key, "let us", value )
+        
+
